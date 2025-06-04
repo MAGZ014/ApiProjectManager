caf7d9daee123e56f7ffe91f0f94c89534fc68c2
Manuel Alejandro <elgtorner10fega@gmail.com>
2025-06-03 21:50:53 -0600

    feat(auth): add JWT token and user details services

--- src/main/java/com/projectmanager/projectmanager/service/CustomUserDetailsService.java
+++ src/main/java/com/projectmanager/projectmanager/service/CustomUserDetailsService.java
@@ -1,4 +1,23 @@
 package com.projectmanager.projectmanager.service;
 
-public class CustomUserDetailsService {
+import com.projectmanager.projectmanager.respository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository= userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not Found"));
+    }
 }
--- src/main/java/com/projectmanager/projectmanager/service/JwtService.java
+++ src/main/java/com/projectmanager/projectmanager/service/JwtService.java
@@ -1,4 +1,65 @@
 package com.projectmanager.projectmanager.service;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration-ms}")
+    private long expiration;
+
+    @Value("${jwt.refresh-token-ms}")
+    private long refreshExpiration;
+
+
+
+    private Key getSigninKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+expiration))
+                .signWith(getSigninKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken(String email){
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+refreshExpiration))
+                .signWith(getSigninKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSigninKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser().setSigningKey(getSigninKey()).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
 }
